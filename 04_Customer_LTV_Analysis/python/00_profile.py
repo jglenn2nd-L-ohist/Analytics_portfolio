b78e7a525c7479a7d46dc55e86eacc6ab34dc995
J.Glenn <j.glenn2nd@gmail.com>
2026-07-14 09:54:57 -0400

    Add Data Profile: Python profile EDA script

---
+++ 04_Customer_LTV_Analysis/python/00_profile.py
@@ -0,0 +1,26 @@
+######################################################
+# Project: Customer Lifetime Value Analysis
+# Filename: 00_profile.py
+# Business Question: What are our customer tendencies and what is their lifetime value?
+# Purpose: Determine data irregularities, prepare for data cleaning
+# Author: J.Glenn
+# Date: 2026-07-14
+######################################################
+
+# - Import libraries
+import pandas as pd
+import numpy as np
+
+# - Import data set
+uci = pd.read_csv("../data/UCIOnlineRetail.csv")
+
+# - Preview data
+print(uci.shape)
+print(uci.describe())
+print(uci.info())
+
+# - Duplicate check
+print("duplicate rows:", uci.duplicated().sum())
+
+# - Cancellation check
+print("Cancellations:", uci['InvoiceNo'].str.startswith('C').sum())
