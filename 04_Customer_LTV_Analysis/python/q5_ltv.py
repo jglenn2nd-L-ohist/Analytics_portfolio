44c6975216b0331f6e0b64116fc1b09617749c18
J.Glenn <j.glenn2nd@gmail.com>
2026-07-20 14:41:40 -0400

    Update Python and Viz: Update python script to include overall cohort averages (3m min) and include horizontal bar chart

--- 04_Customer_LTV_Analysis/python/q5_ltv.py
+++ 04_Customer_LTV_Analysis/python/q5_ltv.py
@@ -31,13 +31,25 @@ ltv["period"] = (ltv["InvoiceDate"] - ltv["cohort"]).apply(lambda x: x.n)
 # - Determine cohort avg revenue
 avg = ltv.groupby(["cohort","period"])["revenue"].mean()
 avg_mat = avg.unstack()
+avg_mat = avg_mat[avg_mat.count(axis=1) >= 3]
+t_avg = avg_mat.mean(axis=1)
+t_avg = t_avg[t_avg.index != pd.Period("2011-05", "M")] 
 
-# - Create  heatmap
+# - Create  heatmap for matrix
 fig, ax = plt.subplots(figsize=(11,6))
-sns.heatmap(avg_mat, annot=True, fmt="0.0f", cmap="Blues", ax=ax, vmax=50)
+sns.heatmap(avg_mat, annot=True, fmt=".0f", cmap="Blues", ax=ax, vmax=50)
 
 plt.title("Cohort average revenue per month")
 plt.tight_layout()
 
 plt.savefig("../outputs/q5_ltv.png")
+
+# - Create chart for total average cohort spend
+fig, ax = plt.subplots(figsize=(10,6))
+
+ax.barh(t_avg.index.astype(str), t_avg.values, label="Avg Revenue/Month By Cohort")
+ax.set_xlabel("Revenue generated")
+ax.set_title("Avg Revenue by Cohort by Month - Excluding 2011-05 Outlier")
+
+plt.savefig("../outputs/q5b_ltv_avg.png")
 plt.show()
