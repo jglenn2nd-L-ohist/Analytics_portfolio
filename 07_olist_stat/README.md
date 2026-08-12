65b9b2d8aef3b7d6a53068e0573a9a7374f11ec9
J.Glenn <j.glenn2nd@gmail.com>
2026-08-12 00:30:02 -0400

    Add Python script: q2 Python script to determine distances and delivery correlation; Update README files, findings, limitations

--- 07_olist_stat/README.md
+++ 07_olist_stat/README.md
@@ -35,10 +35,16 @@ Date Range:
 | Q1 | Early/On-time rate: 92.08% · Late rate: 7.92% · Average days late: 10 · Average days early: 13 | 
 | Q2 | States with fewer than 50 shipments excluded; sample too small to distinguish 
 performance from noise · MA worst on-time rate at 77% · SP (78,598), MG (8,601), 
-PR (8,487) all sit at 92-95% · MA underperforms high-volume peers by 15+ points (Distance correlation portion will be answered via the python diagnostic phase) |
+PR (8,487) all sit at 92-95% · MA underperforms high-volume peers by 15+ points;     
+  -Distance correlation coefficient .077 shows there is effectively no correlation between the distance and the delivery arrival |
 | Q3 |  4 or more days late review scores drop by more than 1.5 points; 4 or more (1.93) 1-3 (3.72); Early arrivals have 4.3 rating on +87,000 reviews; On-time total reviews (1,461) v early (87.375) shows a trend of conservative estimated delivery dates |
 | Q4 | Filtered out 1 category that had NULL value; No visible correlation beween freight ratio and reviews; scatterplot and correlation coeffficient to be done in next phase of analysis; Noteworthy: review scores had to be assigned to categories and orders. With that some orders having multiple categories are represented in the output | 
 | Q6 | Determined that the peak season on-time rate dips to 88.47% (November-December) This marks an OTR drop of app 3.5 points from the normal rate of 92.08% |
+--
+
+## Limitations
+
+Upon joining all the tables, 3,000 entries were found to have zip prefixes not found in the loca table. They were dropped due to mismatch. 3,000 entries represents under 2% of the dataset
 --
 
 ## Files
---
+++ 07_olist_stat/python/q2_diestance_delay.py
@@ -0,0 +1,75 @@
+#########################################################
+## Project: Olist E-commerce Fulfillment Analysis
+## File name: q2_distance_delay.py
+## Business question:  Q2) Which seller states have the 
+##                     worst on-time delivery rates, and 
+##                     does seller-customer geographic 
+##                     distance correlate with delay? 
+## Purpose:   Determine worst on-time rates by state
+##            and see is there a distance correlation
+## Author: J.Glenn
+## Date: August 2026
+#########################################################
+
+# - import libraries
+import pandas as pd
+import numpy as np
+import sqlite3 as sq
+import math as m
+
+# - import data set
+conn = sq.connect("../data/olist.db")
+
+# - query data
+query = """
+        SELECT
+            c.customer_id AS Cust_id
+        ,   o.order_id AS order_id
+        ,	o.order_item_id AS order_item_id
+        ,	c.customer_zip_code_prefix AS Cust_zip
+        , 	l.geolocation_lat AS Cust_lat
+        ,	l.geolocation_lng AS Cust_long
+        ,	v.seller_id AS Seller_id
+        ,	v.seller_zip_code_prefix AS Seller_zip 
+        ,	l2.geolocation_lat AS Seller_lat
+        ,	l2.geolocation_lng AS Seller_long
+        ,   p.order_estimated_delivery_date AS Estimated_date
+        ,   p.order_delivered_customer_date AS Actual_date
+        ,	ROUND(julianday(p.order_delivered_customer_date)-julianday(p.order_estimated_delivery_date),0) arrival_diff
+   
+        FROM
+            cust c
+        JOIN
+            loca l
+        on c.customer_zip_code_prefix = l.geolocation_zip_code_prefix
+        JOIN
+            purch p
+        on c.customer_id = p.customer_id
+        JOIN
+            oitem o
+        on p.order_id = o.order_id
+        JOIN
+            vend v
+        on o.seller_id = v.seller_id
+        JOIN
+            loca l2
+        on v.seller_zip_code_prefix = l2.geolocation_zip_code_prefix
+"""
+distance = pd.read_sql_query(query,conn)
+
+conn.close()
+
+# - function to return distance between buyer and seller
+def haversine(Cust_lat,Cust_long,Seller_lat,Seller_long):
+    R = 6371
+    lat1 = m.radians(Cust_lat)
+    lat2 = m.radians(Seller_lat)
+    dlat = m.radians(Seller_lat - Cust_lat)
+    dlng = m.radians(Seller_long - Cust_long)
+    a = m.sin(dlat/2)**2 + m.cos(lat1) * m.cos(lat2) * m.sin(dlng/2)**2
+    c = 2 * m.atan2(m.sqrt(a), m.sqrt(1-a))
+    return R * c 
+
+distance["km_dist"] = distance.apply(lambda row: haversine(row["Cust_lat"], row["Cust_long"], row["Seller_lat"], row["Seller_long"]), axis=1)
+
+print(distance[["km_dist","arrival_diff"]].corr())
